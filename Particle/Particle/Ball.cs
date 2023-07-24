5935977c91f2fbe6a2e9caf125c7916be7060c6c
Kni9t <clone111@bk.ru>
2023-07-24 14:30:47 +0300

    Balls class

--- Particle/Particle/Ball.cs
+++ Particle/Particle/Ball.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace Particle
 {
-    internal class Ball
+    class Ball
     {
+        Ellipse Body;
+        public Ball(Brush Color, double Size = 8)
+        {
+            Body = new Ellipse();
+            Body.Height = 8;
+            Body.Width = Body.Height;
+            Body.Fill = Color;
+        }
     }
 }
--- Particle/Particle/MainWindow.xaml.cs
+++ Particle/Particle/MainWindow.xaml.cs
@@ -15,14 +15,13 @@ using System.Windows.Shapes;
 
 namespace Particle
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            MainGrid.Background = Brushes.Black;
         }
     }
 }
