e45562113dfaf74339cd18eea1518284d36ddbd9
Kni9t <clone111@bk.ru>
2023-07-24 15:27:56 +0300

    Work ball class

---
+++ ParticlSim/ParticlSim/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ParticlSim
+{
+    public class Ball
+    {
+        Brush Color;
+        int X, Y, Size;
+
+        public Ball(Brush Color, int Size, int X = 50, int Y = 50)
+        {
+            this.Color = Color;
+            this.Size = Size;
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public void Print(Graphics G)
+        {
+            G.FillEllipse(Color, X, Y, Size, Size);
+        }
+    }
+}
--- ParticlSim/ParticlSim/Form1.cs
+++ ParticlSim/ParticlSim/Form1.cs
@@ -12,10 +12,19 @@ namespace ParticlSim
 {
     public partial class Form1 : Form
     {
+        static Random R = new Random();
         public Form1()
         {
             InitializeComponent();
             pictureBox1.BackColor = Color.Black;
+            Ball b = new Ball(Brushes.Red, 10, R.Next(pictureBox1.Width), R.Next(pictureBox1.Height));
+
+            Bitmap BitMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics G = Graphics.FromImage(BitMap);
+
+            b.Print(G);
+
+            pictureBox1.Image = BitMap;
         }
     }
 }
